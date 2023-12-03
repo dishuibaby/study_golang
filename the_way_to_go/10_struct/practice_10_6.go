3743c93aaf467b20205b9e99d61eb506a9f77c6c
Dishuibaby <dishuibaby@gmail.com>
2023-12-03 21:29:33 +0800

    struct 10.1-10.5
    Signed-off-by: Dishuibaby <dishuibaby@gmail.com>

---
+++ the_way_to_go/10_struct/practice_10_6.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type employee struct {
+	salary float32
+}
+
+// 按比例发放工资
+func (e *employee) giveRaise(pt float32) float32 {
+	salary := e.salary * pt
+	return salary
+}
+
+func main() {
+	xiaoming := &employee{1123467.43}
+	salary := xiaoming.giveRaise(0.95)
+	fmt.Printf("xiaoming salary is : %.2f\n", salary)
+
+	xiaowang := &employee{9999999.43}
+	salary = xiaowang.giveRaise(1.1)
+	fmt.Printf("xiaowang salary is : %.2f\n", salary)
+}
